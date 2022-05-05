d467d4c423ae8a569dea76fef59f980dc77a2ecd
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-05 23:05:24 +0200

    No commit

--- js/featured-products.js
+++ js/featured-products.js
@@ -10,36 +10,33 @@ const productsUrl = baseUrl + "api/products?populate=*";
   try {
     const response = await fetch(productsUrl);
     const json = await response.json();
-
     container.innerHTML = "";
 
     for (let i = 0; i < json.data.length; i++) {
       console.log(json.data[i].attributes.image.data[0].attributes.url);
-
       let displayOption = "none";
 
       if (json.data[i].attributes.featured) {
         displayOption = "block";
       }
-      // console.log(json.data[i].attributes.featured);
-
-      console.log(displayOption);
 
       const product = json.data[i];
-      console.log(json.data[i].attributes.image.data[0].attributes.url);
       container.innerHTML += `
       <a href="product-specific.html?id=${product.id}" style="display: ${displayOption}">
-        <img src="http://localhost:1337${product.attributes.image.data[0].attributes.url}" alt=${product.attributes.title}>
+        <img src="https://pawn-api.herokuapp.com${product.attributes.image.data[0].attributes.url}" alt=${product.attributes.title}>
         <h3>${product.attributes.title}</h3>
         <p>${product.attributes.description}</p>
         <p>${product.attributes.price},-</p>
-        <p>${product.attributes.featured}</p>
       </a>`;
     }
   }
+
   catch (error) {
+
     console.log(error);
+
     displayMessage("error", error, ".featured-products-section");
+
   }
 
 })();
