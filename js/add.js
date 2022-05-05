8184cc5c9842f59e6a0f84c47d9449123a91377f
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-05 22:15:22 +0200

    No commit

--- js/add.js
+++ js/add.js
@@ -34,16 +34,20 @@ function submitForm(event) {
   const priceValue = parseFloat(price.value);
   const descriptionValue = description.value.trim();
   const featuredValue = isFeatured;
+  const imageValue = image.files
 
   if (titleValue.length === 0 || priceValue.length === 0 || isNaN(priceValue) || descriptionValue.length === 0) {
     return displayMessage("warning", "Please supply proper values", ".message-container");
   };
 
-  addProduct(titleValue, priceValue, descriptionValue, featuredValue);
+  addProduct(titleValue, priceValue, descriptionValue, featuredValue, imageValue);
 }
 
-async function addProduct(title, price, description, featured) {
+async function addProduct(title, price, description, featured, image) {
   const url = baseUrl + "api/products";
+  const filesAdded = new FormData();
+
+  filesAdded.append("files.image", image[0]);
 
   const data = JSON.stringify({
     data: {
@@ -51,6 +55,7 @@ async function addProduct(title, price, description, featured) {
       price: price,
       description: description,
       featured: featured,
+      image: image,
     }
   })
 
@@ -80,47 +85,10 @@ async function addProduct(title, price, description, featured) {
     location.href = "products.html";
 
   }
+
   catch (error) {
     console.log(error);
     displayMessage("error", "An error occured", ".message-container");
-
   }
 
-}
-
-
-/////////////////////////////////////
-async function addProduct(data, cover_image, productImages) {
-
-  const updateUrl = baseUrl + "products"
-  const formData = new FormData();
-
-  formData.append("files.cover_image", cover_image[0]);
-  formData.append("files.images", productImages[0])
-  formData.append("data", data);
-
-  const options = {
-    method: "POST",
-    body: formData,
-    headers: {
-      // "Content-Type": "application/json",
-
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  try {
-    const response = await fetch(updateUrl, options);
-    const json = await response.json();
-
-    console.log(json)
-
-    if (json.error) {
-      console.log("error")
-    }
-    loader.innerHTML = "Loading"
-
-  } catch (error) {
-    console.log(error)
-  }
 }
--- js/featured-products.js
+++ js/featured-products.js
@@ -13,26 +13,43 @@ const productsUrl = baseUrl + "api/products?populate=*";
 
     container.innerHTML = "";
 
-    const featured = true;
 
-    // console.log(json);
-    console.log(json.data[0].attributes.featured);
-    // if (json.data[0].attributes.featured) {
 
-    // }
 
-    json.data.forEach(function (product) {
+    // json.data.forEach(function (product) {
+
+    // container.innerHTML += `
+    //     <a href="product-specific.html?id=${product.id}" style="display: ">
+    //       <img src="http://localhost:1337${product.attributes.image.data[0].attributes.url}" alt=${product.attributes.title}>
+    //       <h3>${product.attributes.title}</h3>
+    //       <p>${product.attributes.description}</p>
+    //       <p>${product.attributes.price},-</p>
+    //       <p>${product.attributes.featured}</p>
+    //     </a>`;
+    // });
+
+    for (let i = 0; i < json.data.length; i++) {
+      console.log(json.data[i].attributes.image.data[0].attributes.url);
+
+      let displayOption = "none";
+
+      if (json.data[i].attributes.featured) {
+        displayOption = "block";
+      }
+      // console.log(json.data[i].attributes.featured);
+
+      console.log(displayOption);
+
+      const product = json.data[i];
       container.innerHTML += `
-        <a href="product-specific.html?id=${product.id}">
-          <img src="http://localhost:1337${product.attributes.image.data[0].attributes.url}" alt=${product.attributes.title}>
-          <h3>${product.attributes.title}</h3>
-          <p>${product.attributes.description}</p>
-          <p>${product.attributes.price},-</p>
-        </a>`;
-    });
-
-    console.log(json.data);
-
+      <a href="product-specific.html?id=${product.id}" style="display: ${displayOption}">
+        <img src="http://localhost:1337${product.attributes.image.data[0].attributes.url}" alt=${product.attributes.title}>
+        <h3>${product.attributes.title}</h3>
+        <p>${product.attributes.description}</p>
+        <p>${product.attributes.price},-</p>
+        <p>${product.attributes.featured}</p>
+      </a>`;
+    }
   }
   catch (error) {
     console.log(error);
