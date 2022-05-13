9c3eb0219e8611db2a444d87dabacee9dce8ff8f
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-14 00:22:58 +0200

    No commit

--- javascript/components/common/footerAdmin.js
+++ javascript/components/common/footerAdmin.js
@@ -10,26 +10,6 @@ footer.innerHTML =
         <li class="footer__admin__div__ul-li">Edit guide</li>
       </ul>
     </div>
-
-    <div class="footer__admin__div">
-      <h4 class="footer__admin__div__h4">Help</h4>
-      <ul class="footer__admin__div__ul">
-        <li class="footer__admin__div__ul-li">Admin help center</li>
-        <li class="footer__admin__div__ul-li">Terms of use</li>
-        <li class="footer__admin__div__ul-li">Service guide</li>
-        <li class="footer__admin__div__ul-li">Edit guide</li>
-      </ul>
-    </div>
-    
-    <div class="footer__admin__div">
-      <h4 class="footer__admin__div__h4">Help</h4>
-      <ul class="footer__admin__div__ul">
-        <li class="footer__admin__div__ul-li">Admin help center</li>
-        <li class="footer__admin__div__ul-li">Terms of use</li>
-        <li class="footer__admin__div__ul-li">Service guide</li>
-        <li class="footer__admin__div__ul-li">Edit guide</li>
-      </ul>
-    </div>
     
     <div class="footer__admin__div">
       <h4 class="footer__admin__div__h4">Help</h4>
