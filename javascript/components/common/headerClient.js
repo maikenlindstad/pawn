2a77ab88b7007297c8025d0e8b61ea33f2480e79
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-18 20:46:47 +0200

    No commit

---
+++ javascript/components/common/headerClient.js
@@ -0,0 +1,58 @@
+const header = document.querySelector(".client__header");
+const categoryBanner = document.querySelector(".category-banner");
+
+header.innerHTML = `
+<div class="client__header__div">
+        <label for="hamburger-menu"><i class="icon fa-solid fa-bars"></i></label>
+        <input type="checkbox" name="hamburger-menu" id="hamburger-menu">
+
+
+        <nav class="client__header__div__nav">
+
+          <ul class="client__header__div__nav__ul">
+
+            <li class="client__header__div__nav__ul__li"><a href="index.html" class="client__header__div__nav__ul__li__a active">Home</a></li>
+            <li class="client__header__div__nav__ul__li"><a href="products.html" class="client__header__div__nav__ul__li__a">Products</a></li>
+            <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">About us</a></li>
+            <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">Contact</a></li>
+
+          </ul>
+
+        </nav>
+
+      </div>
+
+      <div>
+        <a href="index.html">
+          <img src="images/logo.png" alt="Pawn.com logo" style="width:100px; margin: 0;">
+        </a>
+      </div>
+
+      <div>
+      <a href="login.html"><i class="icon fa-solid fa-user"></i></a>
+        <a href="cart.html"><i class="icon fa-solid fa-cart-shopping"></i></a>
+      </div>`;
+
+categoryBanner.innerHTML = `
+<div>
+<div class="category-banner-div">
+  <p>Books</p>
+</div>
+<div class="category-banner-div">
+  <p>Chess sets</p>
+</div>
+<div class="category-banner-div">
+  <p>Interior</p>
+</div>
+</div>
+<div>
+<div class="category-banner-div">
+  <p>Magazines</p>
+</div>
+<div class="category-banner-div">
+  <p>Courses</p>
+</div>
+<div class="category-banner-div">
+  <p>Fide.com</p>
+</div>
+</div>`;
--- js/products-list.js
+++ js/products-list.js
@@ -1,7 +1,7 @@
 import { baseUrl } from "./settings/api.js";
 import displayMessage from "./components/common/displayMessage.js";
-import createMenu from "./components/common/createMenu.js";
-createMenu();
+// import createMenu from "./components/common/createMenu.js";
+// createMenu();
 
 // const queryString = document.location.search;
 // const params = new URLSearchParams(queryString);
@@ -14,7 +14,7 @@ const productUrl = baseUrl + "products/?populate=*";
     const response = await fetch(productUrl);
     const json = await response.json();
 
-    const productsContainer = document.querySelector(".product-list");
+    const productsContainer = document.querySelector(".products-section");
     productsContainer.innerHTML = "";
 
     for (let i = 0; i < json.data.length; i++) {
@@ -24,29 +24,19 @@ const productUrl = baseUrl + "products/?populate=*";
       console.log(product);
       console.log(product.image.data[0].attributes.url);
       productsContainer.innerHTML += `
-      <img src="https://pawn-api.herokuapp.com${product.image.data[0].attributes.url}" alt=${product.title}>
-      <h2>${product.title}</h2>
-      <p>${product.description}</p>
-      <p>${product.price}</p>
+      <a href="product-specific.html?id=${product.id}" class="product-card">
+        <div class="product-image" style="background-size: cover; background-repeat: no-repeat;height: 380px; width: 220px; background-image: url(https://pawn-api.herokuapp.com${product.image.data[0].attributes.url})">
+        </div>
+        <div class="product-information">
+          <h3>${product.title}</h3>
+          <p>${product.price},-</p>
+        </div>
+        <div class="product-button-div">
+          <p class="product-button">View</p>
+        </div>
+      </a>
       `;
 
-      //   container.innerHTML += `
-      //   <a href="product-specific.html?id=${product.id}" style="display: ${displayOption}">
-      // <img src="https://pawn-api.herokuapp.com${product.image.data[0].attributes.url}" alt=${product.attributes.title}>
-      //     
-      //     <h3>${product.attributes.title}</h3>
-      //     <p>${product.attributes.description}</p>
-      //     <p>${product.attributes.price},-</p>
-      //   </a>`;
-      // }
-
-      // productsContainer.innerHTML += `
-
-      // <h2>${product.data[i].attributes.title}</h2>`;
-
-      // <img src="https://pawn-api.herokuapp.com${details.data.attributes.image.data[0].attributes.url}" alt="${details.data.attributes.title}">
-      //   
-      //   <p>${details.data.attributes.description}</p>`;
     }
 
 
