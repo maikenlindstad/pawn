26c8c6f70e10aa7d137122f38f921e31746d49f7
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-21 14:54:34 +0200

    No commit

--- javascript/components/common/footerClient.js
+++ javascript/components/common/footerClient.js
@@ -19,24 +19,4 @@ footer.innerHTML =
         <li class="footer__client__div__ul-li">Return policy</li>
         <li class="footer__client__div__ul-li">Cookies</li>
       </ul>
-    </div>
-    
-    <div class="footer__client__div">
-      <h4 class="footer__client__div__h4">Help</h4>
-      <ul class="footer__client__div__ul">
-        <li class="footer__client__div__ul-li">Customer service</li>
-        <li class="footer__client__div__ul-li">Terms of use</li>
-        <li class="footer__client__div__ul-li">Return policy</li>
-        <li class="footer__client__div__ul-li">Cookies</li>
-      </ul>
-    </div>
-    
-    <div class="footer__client__div">
-      <h4 class="footer__client__div__h4">Help</h4>
-      <ul class="footer__client__div__ul">
-        <li class="footer__client__div__ul-li">Customer service</li>
-        <li class="footer__client__div__ul-li">Terms of use</li>
-        <li class="footer__client__div__ul-li">Return policy</li>
-        <li class="footer__client__div__ul-li">Cookies</li>
-      </ul>
     </div>`;
