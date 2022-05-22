2b867b3b5caea550b5edf0aa5def6e25f2b69892
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-22 18:53:34 +0200

    No commit

--- javascript/components/common/headerClient.js
+++ javascript/components/common/headerClient.js
@@ -1,36 +1,36 @@
-const header = document.querySelector(".client__header");
+// const header = document.querySelector(".client__header");
 const categoryBanner = document.querySelector(".category-banner");
 
-header.innerHTML = `
-<div class="client__header__div">
-        <label for="hamburger-menu"><i class="icon fa-solid fa-bars"></i></label>
-        <input type="checkbox" name="hamburger-menu" id="hamburger-menu">
+// header.innerHTML = `
+// <div class="client__header__div">
+//         <label for="hamburger-menu"><i class="icon fa-solid fa-bars"></i></label>
+//         <input type="checkbox" name="hamburger-menu" id="hamburger-menu">
 
 
-        <nav class="client__header__div__nav">
+//         <nav class="client__header__div__nav">
 
-          <ul class="client__header__div__nav__ul">
+//           <ul class="client__header__div__nav__ul">
 
-            <li class="client__header__div__nav__ul__li"><a href="index.html" class="client__header__div__nav__ul__li__a active">Home</a></li>
-            <li class="client__header__div__nav__ul__li"><a href="products.html" class="client__header__div__nav__ul__li__a">Products</a></li>
-            <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">About us</a></li>
-            <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">Contact</a></li>
+//             <li class="client__header__div__nav__ul__li"><a href="index.html" class="client__header__div__nav__ul__li__a active">Home</a></li>
+//             <li class="client__header__div__nav__ul__li"><a href="products.html" class="client__header__div__nav__ul__li__a">Products</a></li>
+//             <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">About us</a></li>
+//             <li class="client__header__div__nav__ul__li"><a href="" class="client__header__div__nav__ul__li__a">Contact</a></li>
 
-          </ul>
+//           </ul>
 
-        </nav>
+//         </nav>
 
-      </div>
+//       </div>
 
-      <div>
-        <a href="index.html">
-          <img src="images/logo.png" alt="Pawn.com logo" style="width:100px; margin: 0;">
-        </a>
-      </div>
+//       <div>
+//         <a href="index.html">
+//           <img src="images/logo.png" alt="Pawn.com logo" style="width:100px; margin: 0;">
+//         </a>
+//       </div>
 
-      <div>
-        <a href="cart.html"><i class="icon fa-solid fa-cart-shopping"></i></a>
-      </div>`;
+//       <div>
+//         <a href="cart.html"><i class="icon fa-solid fa-cart-shopping"></i></a>
+//       </div>`;
 
 categoryBanner.innerHTML = `
 <div>
--- js/components/common/createClientMenu.js
+++ js/components/common/createClientMenu.js
@@ -5,14 +5,14 @@ export default function createTopMenu() {
   const username = getUserName();
   const publicMenu = document.querySelector(".menu-container-public");
 
-  let authLink = `<a href="login.html" class="${pathname === '/login.html' ? 'active' : '' || pathname === '/client/products.html' ? 'active' : ''}">Business</a>`;
+  let authLink = `<a href="login.html" class="${pathname === '/login.html' ? 'activeSectionHeader' : '' || pathname === '/client/products.html' ? 'activeSectionHeader' : ''}">Business</a>`;
   if (username) {
     authLink = `<a href="admin/welcome.html">Back to Dashboard</a>`;
   }
 
   publicMenu.innerHTML = `
     ${authLink}
-    <a href="/" class="${pathname === '/index.html' ? 'active' : '' || pathname === '/products.html' ? 'active' : '' || pathname === '/product-specific.html' ? 'active' : '' || pathname === '/cart.html' ? 'active' : ''}">Private</a>
+    <a href="/" class="${pathname === '/index.html' ? 'activeSectionHeader' : '' || pathname === '/products.html' ? 'activeSectionHeader' : '' || pathname === '/product-specific.html' ? 'activeSectionHeader' : '' || pathname === '/cart.html' ? 'activeSectionHeader' : ''}">Private</a>
     `;
 };
 
